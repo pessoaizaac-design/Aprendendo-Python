ac3efb00758da09d2d2e75fc9317c4c6649aa5ad
Higor <pessoaizaac@gmail.com>
2026-05-20 14:52:53 -0300

    Agregação

---
+++ Otavio/agregação/agregacao/main.py
@@ -0,0 +1,16 @@
+from agregacao import Produto
+from agregacao import CarrinhoDeCompras
+
+carrinho = CarrinhoDeCompras()
+
+p1 = Produto('Camiseta', 199)
+p2 = Produto('Iphone', 10000)
+p3 = Produto('Caneca', 50)
+
+carrinho.inserir_produto(p1)
+carrinho.inserir_produto(p2)
+carrinho.inserir_produto(p3)
+
+carrinho.lista_produto()
+print(carrinho.soma_total())
+
