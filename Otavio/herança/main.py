7cd4cca0a763565bc46379653a944c62b6a7967b
Higor <pessoaizaac@gmail.com>
2026-05-20 16:04:18 -0300

    Executando os testes de herança

---
+++ Otavio/herança/main.py
@@ -0,0 +1,11 @@
+from heranca import *
+
+c1 = Cliente('Luiz', 45)
+print(c1.nome, c1.idade)
+c1.falar('Roupas')
+c1.comprar('Louças')
+
+a1 = Aluno('Maria', 16)
+print(a1.nome, a1.idade)
+a1.falar("POO")
+a1.estudar('Física')
