225424f27465cef05eaf99cf5c9b9d74850c7960
Higor <pessoaizaac@gmail.com>
2026-05-21 09:32:43 -0300

    Dataclasses

---
+++ Otavio/dtcs/main.py
@@ -0,0 +1,11 @@
+from dtcs import *
+
+if __name__ == "__main__":
+    jhon = Person('Jhon', 'Doe', True, ['R1'], 'Jhon Doe')
+    mary = Person('Mary', 'Rose', True, ['R1'])
+
+    print(jhon)
+    print(mary)
+    
+
+   
