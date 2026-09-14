6fc848a8d573b873bdedc453621d55fe5d944f09
Higor <pessoaizaac@gmail.com>
2026-09-14 10:00:38 -0300

    POO - Classe

---
+++ POO/Classe/main.py
@@ -0,0 +1,12 @@
+from POO.Classe.POO1 import Pessoa
+
+p1 = Pessoa('Higor', 18)
+
+p1.comer('Pizza')
+p1.comer('Hambúrguer')
+p1.falar('Python')
+p1.parar_comer()
+p1.falar('Python')
+p1.comer('Churrasco')
+p1.parar_falar()
+p1.parar_falar()
