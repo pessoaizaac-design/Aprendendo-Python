1004a81e653f9399157e17a278ddf0f3d28ea323
Higor <pessoaizaac@gmail.com>
2026-09-15 22:30:48 -0300

    POO - Exercícios

--- POO/Exercícios/001.py
+++ POO/Exercícios/001.py
@@ -1,54 +1,99 @@
+# Métodos de instância
+
+class ContaBancaria:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+
+    def depositar(self,valor):
+        if valor <= 0:
+            print("VALOR INDISPONÍVEL PARA DEPÓSITO")
+        else:
+            print("DEPÓSITO EFETUADO COM SUCESSO")
+            self.saldo += valor
+
+    def sacar(self,valor):
+        if valor > self.saldo:
+            print("SALDO INDISPONÍVEL")
+        elif valor <= 0:
+            print("VALOR INDISPONÍVEL PARA SAQUE")
+        else:
+            print("SAQUE EFETUADO COM SUCESSO")
+            self.saldo -= valor
+
+    def mostrar_saldo(self):
+        print(f'''
+            ---- SALDO --- 
+            TITULAR: {self.titular}
+            SALDO: {self.saldo}
+        ''')
+#-----------------------------------------------------------------------------------------
+
+class Funcionario:
+    def __init__(self, nome, salario, cargo):
+        self.nome = nome
+        self.salario = salario
+        self.cargo = cargo
+
+    def aumentar_salario(self,percentual):
+        if percentual <= 0:
+            print("PERCENTUAL INVÁLIDO PARA AUMENTO")
+        else:
+            self.salario = self.salario + self.salario * (percentual / 100)
+            print("AUMENTO EFETUADO COM SUCESSO")
+
+    def promover(self,novo_cargo, aumento):
+        if self.cargo == novo_cargo:
+            print("CARGO JÁ OCUPADO")
+        else:
+            self.cargo = novo_cargo
+            self.salario = self.salario + self.salario * (aumento / 100)
+            print(f"SEU NOVO CARGO: {novo_cargo} | COM AUMENTO DE: {aumento}")
+
+    def mostrar_informacoes(self):
+        print(f'''
+            ---- FUNCIONÁRIO ----
+            Nome: {self.nome}
+            Cargo: {self.cargo}
+            Salário: {self.salario}
+        ''')
+#-----------------------------------------------------------------------------------------
+
 # Métodos de classe
-total_usuarios = 0
+
+class Produto:
+    produtos = 0
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+        Produto.produtos += 1
+
+    @classmethod
+    def total_produtos(cls):
+        print(f'TOTAL DE PRODUTOS CADASTRADOS: {cls.produtos}')
+#-----------------------------------------------------------------------------------------
 
 class Usuario:
     def __init__(self, nome, email, idade):
         self.nome = nome
-        self.idade = idade
         self.email = email
-
-        Usuario.total_usuarios += 1
-
-    @classmethod
-    def total_usuarios(cls, dados):
-        nome, idade, email = dados.split(',')
-        idade = int(idade)
-        return cls(nome, idade, email)
-
-#-----------------------------------------------------------------------------------------
-
-class Produto:
-    def __init__(self, nome, preco, estoque):
-        self.nome = nome
-        self.preco = preco
-        self.estoque = estoque
+        self.idade = idade
 
     @classmethod
-    def from_string(cls, dados):
-        nome, preco, estoque = dados.split(',')
-        return cls(nome, preco, estoque)
-
-produto_string = "Nootebook, 2500, 100"
-novo_produto = Produto.from_string(produto_string)
-#print(f'''
-    # --- PRODUTO ---
-     #[Nome] -> {novo_produto.nome}
-     #[Preço] -> {novo_produto.preco}
-     #[Estoque] -> {novo_produto.estoque}
-#''')
+    def criar_menor(cls, name, email):
+        return cls(name, email, 17)
 
 #-----------------------------------------------------------------------------------------
 
 # Métodos estáticos
-class Conta:
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
+
+class Validador:
+    def __init__(self):
+        pass
 
     @staticmethod
-    def verificar_cpf(cpf):
+    def validar_cpf(cpf):
         return len(cpf) == 11 and cpf.isdigit()
-
 #-----------------------------------------------------------------------------------------
 
 class Temperatura:
@@ -61,84 +106,49 @@ class Temperatura:
     def para_celsius(temperatura):
         temperatura = (temperatura - 32) / 1.8
         print(f'Essa temperatura em Celsius fica: {temperatura:.2f} C°')
-
-#resultado = Temperatura.para_fahrenheit(30)
-#resultado2 = Temperatura.para_celsius(30)
-
 #-----------------------------------------------------------------------------------------
 
-class ContaBanco:
-    def __init__(self, titular, saldo):
-        self.titular = titular
-        self.saldo = saldo
+class Conta:
+    def __init__(self, saldo_inicial=0):
+        self.saldo = saldo_inicial 
 
-    def depositar(self,valor):
-        if valor < 0:
-            print(f'DEPOSITE UM VALOR POSITIVO!')
-        else:
-            print(f'VALOR DEPOSITADO NO SEU SALDO!')
-            self.saldo += valor
+    @property
+    def saldo(self):
+        return self._saldo
 
-    def sacar(self,valor):
-        if valor > self.saldo:
-            print(f'SALDO INDISPONÍVEL')
-        elif valor < 0:
-            print(f'SAQUE UM VALOR POSITIVO')
-        else:
-            self.saldo -= valor
-
-    def consultar_saldo(self):
-        print(f'''
-            --- SALDO ---
-              R$ {self.saldo}
-        ''')
-
-minha_conta = ContaBanco("Higor", 1000)
-# Teste depósito
-#minha_conta.depositar(500)
-
-# Teste saque
-#minha_conta.sacar(2000)
-#minha_conta.sacar(300)
-
-#Resultado final
-#minha_conta.consultar_saldo()
-
+    @saldo.setter
+    def saldo(self, novo_saldo):
+        if novo_saldo < 0:
+            raise ValueError("O saldo não pode ser negativo.")
+        self._saldo = novo_saldo
 #-----------------------------------------------------------------------------------------
 
-class Compras:
-    def __init__(self):
-        self.produtos = []
+class Product:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
 
-    def adicionar_produto(self, nome, preco):
-        if preco < 0:
-            print(f'PREÇO INVÁLIDO!')
+    @property
+    def preco(self):
+        return self._preco
+
+    @preco.setter
+    def preco(self, novo_preco):
+        if novo_preco < 0:
+            raise ValueError("O SALDO NÃO PODE SER NEGATIVO")
+        elif novo_preco == 0:
+            print("O PREÇO DEVE SER DIFERENTE DE ZERO")
         else:
-            self.produtos.append((nome, preco))
-            print(f'PRODTO ADICIONADO COM SUCESSO!')
+            if hasattr(self, "_preco"):
+                print(f"Preço Anterior: {self._preco}")
+            else:
+                print("Cadrastrando novo preço...")
+            self._preco = novo_preco
+            print(f'Novo Preço: {self._preco}')
 
-    def remover_produto(self,nome):
-        for item in self.produtos:
-            if item[0] == nome:
-                self.produtos.remove[item]
-                print(f'ITEM REMOVIDO COM SUCESSO!')
-                return
-        print('PRODUTO NÃO ENCONTRADO NO CARRINHO')
+produto = Product("Teclado", 150)
 
-    def calcular_total(self):
-        total = 0
-        for k,v in self.produtos:
-            print(f'{k} --> R$ {v:.2f}')
-            total += v
-        print(f'''
-            ---- TOTAL ----
-              R$ {total:.2f}
-        ''')
-
-    def listar_produto(self):
-       print("\n--- PRODUTOS NO CARRINHO ---")
-       if not self.produtos:
-           print("O CARRINHO ESTÁ VAZIO")
-       else:
-           for k in self.produtos:
-               print(f'PRODUTO: {k[0]} | PREÇO R$ {k[1]:.2f}')
+print(produto.preco)
+produto.preco = 180
+print(produto.preco )      
+    
